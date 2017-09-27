013fa48481849531871a10111479a62388048873
czarcasm <droid.harshit@gmail.com>
2017-09-28 00:25:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-var http = require('http');
-var Bot = require('@kikinteractive/kik');
-var request = require('request')
-var answer;
- 
-var bot = new Bot({
- 
-    username: 'master_alfred',
-    apiKey: process.env.API_KEY, 
-    baseUrl: process.env.HEROKU_URL
- 
-});
- 
-bot.updateBotConfiguration();
- 
-bot.onTextMessage((message) => {
- 
-    request('http://api.asksusi.com/susi/chat.json?q=' + encodeURI(query), function(error, response, body) {
- 
-        if (!error && response.statusCode == 200) {
- 
-            answer = JSON.parse(body).answers[0].actions[0].expression;
- 
-        } else {
- 
-            answer = "Oops, Looks like Susi is taking a break, She will be back soon";
- 
+request.post({
+    url: "https://api.kik.com/v1/config",
+    auth: {
+        username: 'master_alfred',
+        pass: process.env.API_KEY
+    },
+    json: {
+        "webhook": "https://master-alfred.herokuapp.com/", 
+        "features": {
+            "receiveReadReceipts": false, 
+            "receiveIsTyping": false, 
+            "manuallySendReadReceipts": false, 
+            "receiveDeliveryReceipts": false
         }
- 
-    });
- 
-    message.reply(answer)
-});
- 
-http.createServer(bot.incoming()).listen(process.env.PORT || 8080)
+    }
+}, callback);
